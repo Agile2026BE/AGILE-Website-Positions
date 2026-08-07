092f85c7f8a09d327a7dc70c02eeb6fc4f61c8d9
Agile2026BE <byronevans2024@gmail.com>
2026-08-07 01:19:01 -0400

    Connect candidate reviews section to homepage

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import ContactSection from "../components/ContactSection";
 import HeroSection from "../components/HeroSection";
 import JobBoard from "../components/JobBoard";
+import ReviewsSection from "../components/ReviewsSection";
 import SiteFooter from "../components/SiteFooter";
 import SiteHeader from "../components/SiteHeader";
 import { jobs } from "../data/jobs";
@@ -11,6 +12,7 @@ export default function HomePage() {
       <SiteHeader />
       <HeroSection />
       <JobBoard jobs={jobs} />
+      <ReviewsSection />
       <ContactSection />
       <SiteFooter />
     </main>
