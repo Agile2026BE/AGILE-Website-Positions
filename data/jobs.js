579538c536acf58d5a2fe5056a5c1173de4b74c0
Agile2026BE <byronevans2024@gmail.com>
2026-08-07 11:20:26 -0400

    Wire verified position details 1091-1100 into Careers dataset

--- data/jobs.js
+++ data/jobs.js
@@ -19,6 +19,7 @@ import details1051To1060 from "./jobDetails/details-1051-1060.js";
 import details1061To1070 from "./jobDetails/details-1061-1070.js";
 import details1071To1080 from "./jobDetails/details-1071-1080.js";
 import details1081To1090 from "./jobDetails/details-1081-1090.js";
+import details1091To1100 from "./jobDetails/details-1091-1100.js";
 
 const coreJobs = [
   ...jobs1001To1020,
@@ -44,6 +45,7 @@ const detailOverlays = [
   ...details1061To1070,
   ...details1071To1080,
   ...details1081To1090,
+  ...details1091To1100,
 ];
 
 const detailsById = new Map(
