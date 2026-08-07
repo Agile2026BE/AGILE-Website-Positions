16c328ca705217f73d9469ebcbc1a3524bddfe04
Agile2026BE <byronevans2024@gmail.com>
2026-08-07 02:41:03 -0400

    Connect show more button module styles

--- components/ShowMorePositionsButton.js
+++ components/ShowMorePositionsButton.js
@@ -1,6 +1,8 @@
+import styles from "./ShowMorePositionsButton.module.css";
+
 export default function ShowMorePositionsButton({ label, onClick }) {
   return (
-    <button type="button" onClick={onClick}>
+    <button className={styles.button} type="button" onClick={onClick}>
       {label}
     </button>
   );
