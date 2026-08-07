9abbb8ba576debfdfbc514d64fc64b874fef16f5
Agile2026BE <byronevans2024@gmail.com>
2026-08-07 09:13:27 -0400

    Wire recovered 170-position dataset into AGILE Careers

--- data/jobs.js
+++ data/jobs.js
@@ -1,5 +1,25 @@
-export const jobs = [];
+import { jobs1001To1020 } from "./jobs/positions-1001-1020.js";
+import { jobs1021To1040 } from "./jobs/positions-1021-1040.js";
+import { jobs1041To1060 } from "./jobs/positions-1041-1060.js";
+import { jobs1061To1080 } from "./jobs/positions-1061-1080.js";
+import { jobs1081To1100 } from "./jobs/positions-1081-1100.js";
+import { jobs1101To1112 } from "./jobs/positions-1101-1112.js";
+import { jobs1113To1132 } from "./jobs/positions-1113-1132.js";
+import { jobs1133To1142 } from "./jobs/positions-1133-1142.js";
+import { jobs1143To1152 } from "./jobs/positions-1143-1152.js";
+import { jobs1153To1162 } from "./jobs/positions-1153-1162.js";
+import { jobs1163To1172 } from "./jobs/positions-1163-1172.js";
 
-// Source-of-truth job records will be added here only after they are verified
-// against the live AGILE Careers site or an exact recovered/exported blueprint.
-// No reconstructed or guessed position data should be committed to this file.
+export const jobs = [
+  ...jobs1001To1020,
+  ...jobs1021To1040,
+  ...jobs1041To1060,
+  ...jobs1061To1080,
+  ...jobs1081To1100,
+  ...jobs1101To1112,
+  ...jobs1113To1132,
+  ...jobs1133To1142,
+  ...jobs1143To1152,
+  ...jobs1153To1162,
+  ...jobs1163To1172,
+];
