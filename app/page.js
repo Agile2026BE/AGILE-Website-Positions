48858652712180fb5da84c17bb8e94b3e558541d
Agile2026BE <byronevans2024@gmail.com>
2026-08-07 01:14:22 -0400

    Connect contact section and footer to homepage

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
+import ContactSection from "../components/ContactSection";
 import HeroSection from "../components/HeroSection";
 import JobBoard from "../components/JobBoard";
+import SiteFooter from "../components/SiteFooter";
 import SiteHeader from "../components/SiteHeader";
 import { jobs } from "../data/jobs";
 
@@ -9,6 +11,8 @@ export default function HomePage() {
       <SiteHeader />
       <HeroSection />
       <JobBoard jobs={jobs} />
+      <ContactSection />
+      <SiteFooter />
     </main>
   );
 }
