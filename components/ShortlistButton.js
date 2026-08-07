c2f957c7a0fe5f0717c07eebf2dc893583df0778
Agile2026BE <byronevans2024@gmail.com>
2026-08-07 02:36:48 -0400

    Connect shortlist button module styles

--- components/ShortlistButton.js
+++ components/ShortlistButton.js
@@ -1,6 +1,9 @@
+import styles from "./ShortlistButton.module.css";
+
 export default function ShortlistButton({ isShortlisted = false, onClick }) {
   return (
     <button
+      className={styles.button}
       type="button"
       onClick={onClick}
       aria-pressed={isShortlisted}
