f268018b204448545d2a2313c72808395492d345
Agile2026BE <byronevans2024@gmail.com>
2026-08-07 03:28:10 -0400

    Connect position search input module styles

--- components/SearchInput.js
+++ components/SearchInput.js
@@ -1,6 +1,9 @@
+import styles from "./SearchInput.module.css";
+
 export default function SearchInput({ value, onChange, placeholder }) {
   return (
     <input
+      className={styles.input}
       type="search"
       value={value}
       onChange={(event) => onChange(event.target.value)}
