675e18ba14a929b938adce5ddd0ffa1fb2b2d0c3
Agile2026BE <byronevans2024@gmail.com>
2026-08-09 01:15:34 -0400

    Add Market Insights to Careers homepage

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import styles from "./page.module.css";
 import ContactSection from "../components/ContactSection";
 import HeroSection from "../components/HeroSection";
 import JobBoard from "../components/JobBoard";
+import MarketInsightsSection from "../components/MarketInsightsSection";
 import ReviewsSection from "../components/ReviewsSection";
 import SiteFooter from "../components/SiteFooter";
 import SiteHeader from "../components/SiteHeader";
@@ -11,15 +12,14 @@ import { jobs } from "../data/jobs";
 export default function HomePage() {
   return (
     <>
-      <a className={styles.skipLink} href="#main-content">
-        Skip to main content
-      </a>
+      <a className={styles.skipLink} href="#main-content">Skip to main content</a>
       <main className={styles.page} id="main-content">
         <SiteHeader />
         <HeroSection />
         <JobBoard jobs={jobs} />
         <ReviewsSection />
         <WhyAgileSection />
+        <MarketInsightsSection />
         <ContactSection />
         <SiteFooter />
       </main>
