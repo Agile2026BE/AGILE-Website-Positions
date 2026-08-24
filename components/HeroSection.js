512667b7189c702e5447958bb0c56518a9d9d803
Agile2026BE <byronevans2024@gmail.com>
2026-08-23 23:58:03 -0400

    3 Panel Photo Changes

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -103,13 +103,35 @@ export default function HeroSection() {
         </div>
 
         <figure className={`${styles.storyBand} ${styles.desktopStoryBand}`}>
-          <Image
-            className={styles.storyPoster}
-            src="/agile-informed-move-storyboard-v1.png"
-            alt="Professionals moving through a city, reviewing engineering plans, and considering a skyline before a meeting."
-            fill
-            sizes="(max-width: 640px) calc(100vw - 32px), (max-width: 1100px) calc(100vw - 64px), 1000px"
-          />
+          <div className={styles.storyRow}>
+            <div className={`${styles.storyPanel} ${styles.storyPanelLeft}`}>
+              <Image
+                className={styles.storyPanelImg}
+                src="/agile-story-left.jpg"
+                alt="Professionals moving through a city on their way to work."
+                fill
+                sizes="(max-width: 1300px) 30vw, 340px"
+              />
+            </div>
+            <div className={`${styles.storyPanel} ${styles.storyPanelCenter}`}>
+              <Image
+                className={styles.storyPanelImg}
+                src="/agile-story-center.jpg"
+                alt="A team reviewing engineering plans together in a conference room."
+                fill
+                sizes="(max-width: 1300px) 44vw, 500px"
+              />
+            </div>
+            <div className={`${styles.storyPanel} ${styles.storyPanelRight}`}>
+              <Image
+                className={styles.storyPanelImg}
+                src="/agile-story-right.jpg"
+                alt="An executive looking through a contemporary office window while considering his next career move."
+                fill
+                sizes="(max-width: 1300px) 32vw, 370px"
+              />
+            </div>
+          </div>
         </figure>
 
         <figure className={`${styles.storyBand} ${styles.mobileStoryBand}`}>
