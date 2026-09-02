8ea3004ff5d8eea11d904963ab4ae550d8014843
Agile2026BE <byronevans2024@gmail.com>
2026-09-01 20:31:03 -0400

    Add position 1074 (Director of Commissioning) to Featured Positions

--- data/featuredPositionIds.js
+++ data/featuredPositionIds.js
@@ -1,2 +1,2 @@
 // Manually curated "Featured" position IDs, shared by the homepage panel and the careers page button.
-export const featuredPositionIds = ["1010", "1040", "1075", "1129", "1181"];
+export const featuredPositionIds = ["1010", "1040", "1074", "1075", "1129", "1181"];
